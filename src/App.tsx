4f276a7f3ac6b1cf84ef8e0970b231c3eda5bbff
Gus <yoldi.gustavo@gmail.com>
2026-05-23 14:09:16 -0300

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
-import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./index.css";
+
+type APIResponse = {
+  questions: Question[];
+};
+
+type Question = {
+  question: string;
+  options: string;
+  correctOption: number;
+  points: number;
+};
+
+type State = {
+  questions: Question[];
+  //status: loading |
+  status: "loading" | "ready" | "error" | "active" | "finished";
+};
+
+type Action = {
+  payload?: any;
+  type: "dataRecived" | "dataFailed";
+};
+
+const initialState: APIResponse = { questions: [] };
+
+function reducer(state: State, action: Action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown action.type");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http:/localhost:8000/questions")
+      .then((res) => res.json)
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => {
+        console.error(err);
+        dispatch({ type: "dataFail" });
+      });
+  }, []);
+
   return (
     <div>
-      <DateCounter />
+      <div className="app">
+        <Header />
+        <Main>
+          <p>1/15</p>
+          <p>Question</p>
+        </Main>
+      </div>
     </div>
   );
 }
--- src/components/DateCounter.tsx
+++ src/components/DateCounter.tsx
@@ -1,34 +1,58 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+type State = {
+  count: number;
+  step: number;
+};
+
+type Action =
+  | { type: "inc" }
+  | { type: "dec" }
+  | { type: "reset" }
+  | { type: "defineCount"; payload: number }
+  | { type: "defineStep"; payload: number };
+
+const initialState = { count: 0, step: 1 };
+
+/* prettier-ignore */
+function reducer(state: State , action: Action) {
+  switch (action.type) {
+    case 'inc': return {...state, count: state.count + state.step}
+    case 'dec': return {...state, count: state.count - state.step}
+    case 'defineCount': return {...state, count: action.payload}
+    case 'defineStep': return {...state, step: action.payload}
+    case 'reset': return initialState
+
+    default: throw new Error('Unknown action.type')
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "inc" });
   };
 
-  const defineCount = function (e) {
-    setCount(Number(e.target.value));
+  const defineCount = function (e: React.ChangeEvent<HTMLInputElement>) {
+    dispatch({ type: "defineCount", payload: Number(e.target.value) });
   };
 
-  const defineStep = function (e) {
-    setStep(Number(e.target.value));
+  const defineStep = function (e: React.ChangeEvent<HTMLInputElement>) {
+    dispatch({ type: "defineStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
